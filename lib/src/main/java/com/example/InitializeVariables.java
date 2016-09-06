3b893357226440ea5973ebc14b17d1ba16aeff53
danielCatlett <daniel.catlett42@gmail.com>
2016-09-06 13:24:18 -0500

    InitializeVariablesDanielC
    Initializes and prints out variables of various data types

---
+++ lib/src/main/java/com/example/InitializeVariables.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class InitializeVariables
+{
+    public static void main(String[] args)
+    {
+        byte var1=1;
+        short var2=2;
+        int var3=3;
+        long var4=4;
+        float var5=5.5f;
+        double var6=6.6;
+        boolean var7=true;
+        char var8='d';
+        System.out.println(var1+" "+var2+" "+var3+" "+var4+" "+var5+" "+var6+" "+var7+" "+var8);
+    }
+}
